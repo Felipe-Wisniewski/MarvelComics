93d2d1f9942e277d3ddaaaca63c1f74f979466ca
Felipe Wisniewski <felipewisniewski@gmail.com>
2018-07-15 04:38:05 -0300

    layout

--- app/src/main/java/com/felipewisniewski/marvelcomics/comics/ComicsView.java
+++
@@ -1,4 +0,0 @@
-package com.felipewisniewski.marvelcomics.comics;
-
-public interface ComicsView {
-}
--- app/src/main/java/com/felipewisniewski/marvelcomics/marvels/MarvelsView.java
+++
@@ -1,4 +0,0 @@
-package com.felipewisniewski.marvelcomics.marvels;
-
-public interface MarvelsView {
-}
